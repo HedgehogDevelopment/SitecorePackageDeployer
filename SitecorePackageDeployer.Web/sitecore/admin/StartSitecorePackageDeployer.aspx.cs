4eb7467ced42d6f481dd9e639b73da25f519e9f8
Charles Turano <cturano@hhogdev.com>
2015-11-30 15:52:06 -0500

    Made installer start on demand

---
+++ SitecorePackageDeployer.Web/sitecore/admin/StartSitecorePackageDeployer.aspx.cs
@@ -0,0 +1,23 @@
+﻿using Hhogdev.SitecorePackageDeployer.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Hhogdev.SitecorePackageDeployer.Web.sitecore.admin
+{
+    public partial class StartSitecorePackageDeployer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ThreadPool.QueueUserWorkItem((ctx) =>
+            {
+                InstallPackage installer = new InstallPackage();
+                installer.Run();
+            });
+        }
+    }
+}
